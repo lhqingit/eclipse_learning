9b07151d3eee8b1528b66b38fb096d95ad826095
lhqingit <lhq_ing@qq.com>
2018-10-14 10:54:10 +0800

    springboot 静态资源访问，拦截器和消息转化器的配置

--- lhq-springboot/src/main/java/com/lhq/springboot/demo/HelloApplication.java
+++ lhq-springboot/src/main/java/com/lhq/springboot/demo/HelloApplication.java
@@ -1,14 +1,18 @@
 package com.lhq.springboot.demo;
 
+import java.nio.charset.Charset;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@SpringBootApplication
+@SpringBootApplication(exclude= {SolrAutoConfiguration.class})
 //@Configuration
 public class HelloApplication {
 	
@@ -17,8 +21,19 @@ public class HelloApplication {
 	public String hello() {
 		return "hello~第一个springBoot程序~";
 	}
+	
+	@Bean
+	public StringHttpMessageConverter stringHttpMessageConverter() {
+		StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("ISO_8859_1"));
+//		StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+		return converter;
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloApplication.class, args);
+//		SpringApplication.run(HelloApplication.class, args);
+		SpringApplication app = new SpringApplication(HelloApplication.class);
+//		app.setBannerMode(Mode.OFF);
+		app.run(args);
+		
 	}
 }
